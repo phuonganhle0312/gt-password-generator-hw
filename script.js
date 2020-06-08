21f393ba808a17e7b83fecb31b1a233e70c8032a
Phuong Le <phuonganh.le0312@gmail.com>
2020-06-08 18:40:23 -0400

    changed 'number' array to intergers and added arrays to userInput array if true

--- script.js
+++ script.js
@@ -24,8 +24,14 @@ function writePassword() {
     var userInput =[];
     var uppercase =["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"]
     var lowercase =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    var numbers =["1", "2", "3", "4", "5", "6", "7", "8","9"]
+    var intergers =["1", "2", "3", "4", "5", "6", "7", "8","9"]
     var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "/", ",", "."];
+    
+    // add arrays to userInput array if value is true
+    if(uppers){userInput.push.apply(userInput,uppercase)};
+    if(lowers){userInput.push.apply(userInput,lowercase)};
+    if(numbers){userInput.push.apply(userInput, intergers)};
+    if(special){userInput.push.array(userInput, symbols)};
 
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
