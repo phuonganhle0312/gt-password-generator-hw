7a164d2b3d6a6ec626800d86fb3fb3992ad63b20
Phuong Le <phuonganh.le0312@gmail.com>
2020-06-08 18:26:59 -0400

    initialized variables in script.js

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ function writePassword() {
         var numbers = confirm("Would you like numbers in your password?");
         var special = confirm("Would you like special characters in your password?");
     }
+    // initialize variables
+    var userInput =[];
+    var uppercase =["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"]
+    var lowercase =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var numbers =["1", "2", "3", "4", "5", "6", "7", "8","9"]
+    var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "=", "<", ">", "/", ",", "."];
 
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
